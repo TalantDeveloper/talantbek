470a67181ab4f02e56f9c312dd3c280859baeed9
Botirjon Ergashov <139768692+TalantDeveloper@users.noreply.github.com>
2024-11-04 18:08:56 +0500

    save change
    save change

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.service, name='services'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -17,3 +17,10 @@ def welcome(request):
 def index(request):
     return render(request, 'index.html')
 
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def service(request):
+    return render(request, 'service.html')
