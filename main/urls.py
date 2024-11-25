e73aebc144aa78825ecfc46c2a65e060d802fb6a
Botirjon Ergashov <139768692+TalantDeveloper@users.noreply.github.com>
2024-11-25 15:42:44 +0500

    save change
    save change

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    path('services/', views.services, name='services'),
+    path('services/', views.services_views, name='services'),
     path('resume/', views.resume, name='resume'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('contact/', views.contact, name='contact'),
--- main/views.py
+++ main/views.py
@@ -1,21 +1,53 @@
 from django.shortcuts import render
-# from .models import About, Experience, Project, Program
+from .models import About, Science, Service, Page, Card, Skill, AboutMe, Project
 
 
 def welcome(request):
-    return render(request, 'index.html')
+    sciences = Science.objects.first()
+    about = About.objects.first()
+    context = {
+        'sciences': sciences,
+        'about': about,
+    }
+    return render(request, 'index.html', context)
 
 
-def services(request):
-    return render(request, 'services.html')
+def services_views(request):
+    services = Service.objects.all()
+    context = {
+        'services': services,
+    }
+    return render(request, 'services.html', context)
 
 
 def resume(request):
-    return render(request, 'resume.html')
+    experience = Page.objects.first()
+    education = Page.objects.all()[1]
+    skill = Page.objects.all()[2]
+    about_me = Page.objects.all()[3]
+    experiences = Card.objects.filter(page=experience)
+    educations = Card.objects.filter(page=education)
+    skills = Skill.objects.filter(skill=skill)
+    abouts_me = AboutMe.objects.all()
+    context = {
+        'experience': experience,
+        'education': education,
+        'skill': skill,
+        'about_me': about_me,
+        'experiences': experiences,
+        'educations': educations,
+        'skills': skills,
+        'abouts_me': abouts_me,
+    }
+    return render(request, 'resume.html', context)
 
 
 def portfolio(request):
-    return render(request, 'portfolio.html')
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+    }
+    return render(request, 'portfolio.html', context)
 
 
 def contact(request):
